61f91e3c572018253d70272364957d6c1c360e87
Cash Costello <cash.costello@jhuapl.edu>
2021-04-05 19:50:41 -0400

    forgot to update unit tests

--- tests/test_topics.py
+++ tests/test_topics.py
@@ -1,4 +1,4 @@
-import pipeline.query as query
+import pipeline.topics as topics
 
 
 def test_select_text():
@@ -7,6 +7,6 @@ def test_select_text():
             self.fields = fields
 
     mock = Mock(['title', 'desc'])
-    topic = query.Topic('1', 'en', 'title', 'desc', 'narr')
-    text = query.QueryProcessor._select_text(mock, topic)
+    topic = topics.Topic('1', 'en', 'title', 'desc', 'narr')
+    text = topics.TopicProcessor._select_text(mock, topic)
     assert text == "title desc"
