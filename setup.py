5d6887f813efa3dfc93a899785b45f9892bd947d
Cash Costello <cash.costello@jhuapl.edu>
2021-04-22 12:16:10 -0400

    makes patapsco pip installable and updates readme

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("patapsco/__version__.py") as fp:
+    ns = {}
+    exec(fp.read(), ns)
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="patapsco",
+    version=ns['__version__'],
+    description="Cross Language Information Retrieval pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=[
+        "beautifulsoup4",
+        "more-itertools",
+        "pydantic",
+        "pyyaml",
+        "sqlitedict",
+    ],
+    extras_require={
+        "dev": ["pytest", "flake8", "autopep8"]
+    },
+    entry_points={
+        "console_scripts": ["patapsco = patapsco.__main__:main"]
+    }
+)
