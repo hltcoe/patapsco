19c7842fbe189582e830ea0829a233dd23f52171
Cash Costello <cash.costello@jhuapl.edu>
2021-04-28 14:59:53 -0400

    updates unit tests for scorer

--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -1,34 +1,29 @@
 import pathlib
-import patapsco.score as score
-from patapsco import ConfigPreprocessor
-from patapsco import QrelsReaderFactory
-from patapsco import TrecResultsReader
-from patapsco.util.trec import *
-import patapsco.config as config
+
+import pytest
+
+from patapsco.schema import ScoreInputConfig, ScoreConfig
+from patapsco.score import *
 
 
 def test_at_symbol_mapping():
-    directory = pathlib.Path(__file__).parent / 'config_files'
-    path = directory / 'full_config.yml'
-    conf = ConfigPreprocessor.process(path, overrides=None)
-    scorer = score.Scorer(conf.score, qrels=None)
+    config = ScoreConfig(
+        input=ScoreInputConfig(path="test"),
+        metrics=['map', 'P@20'] 
+    )
+    scorer = Scorer(config, qrels=None)
     assert scorer.config.metrics[0] == "map"
     assert scorer.config.metrics[1] == "P_20"    
 
+@pytest.mark.skip(reason="not ready")
 def test_ndcg_prime():
     # TODO complete testing for ndcg_prime
-    directory = pathlib.Path(__file__).parent / 'trec_files'
-    qrel_file = directory / 'qrels_2020'
-    results = directory / 'results.txt'
-    path = pathlib.Path(__file__).parent / 'config_files' / 'full_config.yml'
-    qrel_map = {"format": 'trec', "path": str(qrel_file)}
-    qrels = QrelsReaderFactory.create(qrel_map).read()
-    #qrels_iter = parse_qrels(str(qrel_file))
-    conf = ConfigPreprocessor.process(path, overrides=None)
-    run = TrecResultsReader(results)
-    scorer = score.Scorer(conf.score, qrels)
-    for r in run:
-        scorer.process(r)
-    print(scorer.calc_ndcg_prime())
-
-test_ndcg_prime()
+    config = ScoreConfig(
+        input=ScoreInputConfig(path="test"),
+        metrics=["ndcg'"] 
+    )
+    # create fake qrels to test ndcg', load them, pass to constructor
+    scorer = Scorer(config, qrels=None)
+    scorer.run = []  # fake results (could be in a file or entered here
+    results = scorer.calc_ndcg_prime()
+    #assert something
