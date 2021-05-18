9c2e29b0df2f927f224b3af6c478e7f23d43553b
Cash Costello <cash.costello@jhuapl.edu>
2021-05-18 17:02:52 -0400

    corrects stanza model path for unit tests

--- tests/test_text.py
+++ tests/test_text.py
@@ -17,41 +17,40 @@ def test_stop_words_english_case():
     assert text == ['test']
 
 
-@pytest.mark.slow
-def test_stanza_tokenizer_arabic():
-    tokenizer = StanzaTokenizer(config=None, lang='ar', model_path='~/stanza_resources')
-    tokens = tokenizer.tokenize("في أسرتي ثلاثة أفراد.")
-    assert tokens == ['في', 'أسرتي', 'ثلاثة', 'أفراد', '.']
+class TestStanza:
+    model_path = str(pathlib.Path.home() / 'stanza_resources')
 
+    @pytest.mark.slow
+    def test_stanza_tokenizer_arabic(self):
+        tokenizer = StanzaTokenizer(config=None, lang='ar', model_path=self.model_path)
+        tokens = tokenizer.tokenize("في أسرتي ثلاثة أفراد.")
+        assert tokens == ['في', 'أسرتي', 'ثلاثة', 'أفراد', '.']
 
-@pytest.mark.slow
-def test_stanza_tokenizer_chinese():
-    tokenizer = StanzaTokenizer(config=None, lang='zh', model_path='~/stanza_resources')
-    tokens = tokenizer.tokenize("不但要看,而且要帮。")
-    # jieba is splitting 要看
-    assert tokens == ['不但', '要', '看', ',', '而且', '要', '帮',  '。']
+    @pytest.mark.slow
+    def test_stanza_tokenizer_chinese(self):
+        tokenizer = StanzaTokenizer(config=None, lang='zh', model_path=self.model_path)
+        tokens = tokenizer.tokenize("不但要看,而且要帮。")
+        # jieba is splitting 要看
+        assert tokens == ['不但', '要', '看', ',', '而且', '要', '帮',  '。']
 
+    @pytest.mark.slow
+    def test_stanza_tokenizer_english(self):
+        tokenizer = StanzaTokenizer(config=None, lang='en', model_path=self.model_path)
+        tokens = tokenizer.tokenize("Mary had a little lamb.")
+        assert tokens == ['Mary', 'had', 'a', 'little', 'lamb', '.']
 
-@pytest.mark.slow
-def test_stanza_tokenizer_english():
-    tokenizer = StanzaTokenizer(config=None, lang='en', model_path='~/stanza_resources')
-    tokens = tokenizer.tokenize("Mary had a little lamb.")
-    assert tokens == ['Mary', 'had', 'a', 'little', 'lamb', '.']
+    @pytest.mark.slow
+    def test_stanza_tokenizer_farsi(self):
+        tokenizer = StanzaTokenizer(config=None, lang='fa', model_path=self.model_path)
+        tokens = tokenizer.tokenize("شما بليز رو به فارسی چی میگین؟")
+        assert tokens == ['شما', 'بليز', 'رو', 'به', 'فارسی', 'چی', 'میگین', '؟']
 
-
-@pytest.mark.slow
-def test_stanza_tokenizer_farsi():
-    tokenizer = StanzaTokenizer(config=None, lang='fa', model_path='~/stanza_resources')
-    tokens = tokenizer.tokenize("شما بليز رو به فارسی چی میگین؟")
-    assert tokens == ['شما', 'بليز', 'رو', 'به', 'فارسی', 'چی', 'میگین', '؟']
-
-
-@pytest.mark.slow
-def test_stanza_tokenizer_russian():
-    tokenizer = StanzaTokenizer(config=None, lang='ru', model_path='~/stanza_resources')
-    tokens = tokenizer.tokenize("Я хотел бы пива.")
-    # Does the Russian model not handle punctuation well or did we hit on a bad sentence
-    assert tokens == ['Я', 'хотел', 'бы', 'пива.']
+    @pytest.mark.slow
+    def test_stanza_tokenizer_russian(self):
+        tokenizer = StanzaTokenizer(config=None, lang='ru', model_path=self.model_path)
+        tokens = tokenizer.tokenize("Я хотел бы пива.")
+        # Does the Russian model not handle punctuation well or did we hit on a bad sentence
+        assert tokens == ['Я', 'хотел', 'бы', 'пива.']
 
 
 class TestSpacy:
