02c4e3ebd5df74356c4d3e2812df8a42ac80ee1c
Cash Costello <cash.costello@jhuapl.edu>
2021-05-18 16:34:04 -0400

    using a temp directory for storing scores.txt (also switching to using the jsonl results format

--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -1,8 +1,9 @@
 import pathlib
+import tempfile
 
 import pytest
 
-from patapsco.results import TrecResultsReader
+from patapsco.results import JsonResultsReader
 from patapsco.schema import ScoreInputConfig, ScoreConfig
 from patapsco.score import *
 from patapsco.util.trec import parse_qrels
@@ -19,32 +20,36 @@ def test_at_symbol_mapping():
 
 
 def test_pytrec_eval():
-    directory = pathlib.Path(__file__).parent / 'scoring_files'
-    qrels_path = directory / 'qrels.txt'
-    run_path = directory / 'run.txt'
+    temp_dir = pathlib.Path(tempfile.mkdtemp())
+
+    # score the results
+    base_directory = pathlib.Path(__file__).parent / 'scoring_files'
+    qrels_path = base_directory / 'qrels.txt'
+    results_path = base_directory / 'results.jsonl'
     config = ScoreConfig(
-        input=ScoreInputConfig(path=str(run_path)),
+        input=ScoreInputConfig(path=str(qrels_path)),
         metrics=['map', 'ndcg', 'recall@100']
     )
-    qrels_iter = parse_qrels(str(qrels_path))
-    qrels = next(qrels_iter)
-    scorer = Scorer("test", config, qrels=qrels)
-    results_iter = TrecResultsReader(str(run_path))
+    qrels_data = next(parse_qrels(qrels_path))
+    scorer = Scorer(str(temp_dir), config, qrels=qrels_data)
+    results_iter = JsonResultsReader(results_path)
     for r in results_iter:
         scorer.process(r)
     scorer.end()
-    results_file = pathlib.Path(__file__).parent / 'test/scores.txt'
-    with open(results_file) as f:
-        lines = f.readlines()
-    assert lines[0].split()[0] == 'map'
-    assert lines[1].split()[0] == 'ndcg'
-    assert lines[2].split()[0] == 'recall_100'
+
+    # check that the scores.txt file is correct
+    scores_path = temp_dir / 'scores.txt'
+    data = scores_path.read_text().split("\n")
+    assert data[0].split()[0] == 'map'
+    assert data[1].split()[0] == 'ndcg'
+    assert data[2].split()[0] == 'recall_100'
 
 
 def test_unknown_metric():
     directory = pathlib.Path(__file__).parent / 'scoring_files'
     qrels_path = directory / 'qrels.txt'
     run_path = directory / 'run.txt'
+    results_path = directory / 'results.jsonl'
     config = ScoreConfig(
         input=ScoreInputConfig(path=str(run_path)),
         metrics=["ndcgg"]
@@ -52,25 +57,25 @@ def test_unknown_metric():
     qrels_iter = parse_qrels(str(qrels_path))
     qrels = next(qrels_iter)
     scorer = Scorer("test", config, qrels=qrels)
-    results_iter = TrecResultsReader(str(run_path))
+    results_iter = JsonResultsReader(results_path)
     for r in results_iter:
         scorer.process(r)
     with pytest.raises(ValueError) as e:
-        results = scorer.end()
+        scorer.end()
 
 
 def test_ndcg_prime():
     directory = pathlib.Path(__file__).parent / 'scoring_files'
     qrels_path = directory / 'qrels.txt'
-    run_path = directory / 'run.txt'
+    results_path = directory / 'results.jsonl'
     config = ScoreConfig(
-        input=ScoreInputConfig(path=str(run_path)),
+        input=ScoreInputConfig(path=str(qrels_path)),
         metrics=["ndcg'"]
     )
     qrels_iter = parse_qrels(str(qrels_path))
     qrels = next(qrels_iter)
     scorer = Scorer("test", config, qrels=qrels)
-    results_iter = TrecResultsReader(str(run_path))
+    results_iter = JsonResultsReader(results_path)
     for r in results_iter:
         scorer.process(r)
     results = scorer.calc_ndcg_prime()
