3ddf1bb7ad905bfe3eb5123d43d8930f240d19a0
Eugene Yang <eugene2528@gmail.com>
2021-06-04 11:27:11 -0400

    change removing directory to using shutil.rmtree -- avoiding doing recursive remove by ourselves

--- patapsco/util/file.py
+++ patapsco/util/file.py
@@ -30,13 +30,8 @@ def validate_encoding(encoding):
 
 def delete_dir(path):
     """Recursively delete a directory"""
-    path = pathlib.Path(path)
-    for child in path.glob('*'):
-        if child.is_file():
-            child.unlink()
-        else:
-            delete_dir(child)
-    path.rmdir()
+    from shutil import rmtree
+    rmtree(path)
 
 
 def touch_complete(path):
