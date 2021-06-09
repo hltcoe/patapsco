3e1de000ac20932dcce577b42d2781fe588fa6ad
Cash Costello <cash.costello@jhuapl.edu>
2021-06-09 15:44:57 -0400

    adds python information to grid map reduce jobs

--- patapsco/resources/qsub/map.sh
+++ patapsco/resources/qsub/map.sh
@@ -9,4 +9,10 @@
 # we want zero-based job ids
 JOB_ID=$(($SGE_TASK_ID-1))
 
+DATE=$(date '+%Y-%m-%d %H:%M:%S,%3N')
+PYTHON_VERSION=$(python --version)
+PYTHON_EXE=$(which python)
+echo "$DATE - patapsco-map - INFO - $PYTHON_VERSION"
+echo "$DATE - patapsco-map - INFO - $PYTHON_EXE"
+
 patapsco-map {debug} --stage {stage} --job $JOB_ID --increment {increment} {config}
--- patapsco/resources/qsub/reduce.sh
+++ patapsco/resources/qsub/reduce.sh
@@ -6,4 +6,10 @@
 #$ -l {resources}
 {email}
 
+DATE=$(date '+%Y-%m-%d %H:%M:%S,%3N')
+PYTHON_VERSION=$(python --version)
+PYTHON_EXE=$(which python)
+echo "$DATE - patapsco-reduce - INFO - $PYTHON_VERSION"
+echo "$DATE - patapsco-reduce - INFO - $PYTHON_EXE"
+
 patapsco-reduce {debug} --stage {stage} {config}
