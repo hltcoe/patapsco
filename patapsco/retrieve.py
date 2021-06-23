ce0c8d2c9048909518601f5bfd78680c2ee2df1e
Cash Costello <cash.costello@jhuapl.edu>
2021-06-23 11:29:31 -0400

    adds ability to log query expansion from rm3

--- patapsco/retrieve.py
+++ patapsco/retrieve.py
@@ -50,6 +50,16 @@ class Java:
         self.WhitespaceAnalyzer = jnius.autoclass('org.apache.lucene.analysis.core.WhitespaceAnalyzer')
         self.SimpleSearcher = pyserini.search.SimpleSearcher
 
+        # rm3 logging
+        self.Rm3Reranker = jnius.autoclass('io.anserini.rerank.lib.Rm3Reranker')
+        self.LogManager = jnius.autoclass('org.apache.logging.log4j.LogManager')
+        self.ContextManager = jnius.autoclass('org.apache.logging.log4j.core.LoggerContext')
+        self.Level = jnius.autoclass('org.apache.logging.log4j.Level')
+        self.PatternLayout = jnius.autoclass('org.apache.logging.log4j.core.layout.PatternLayout')
+        self.Charset = jnius.autoclass('java.nio.charset.Charset')
+        self.FileAppender = jnius.autoclass('org.apache.logging.log4j.core.appender.FileAppender')
+        self.cast = jnius.cast
+
 
 class PyseriniRetriever(Task):
     """Use Lucene to retrieve documents from an index"""
@@ -67,6 +77,7 @@ class PyseriniRetriever(Task):
         self._searcher = None
         self.java = Java()
         self.lang = None  # documents language
+        self.rm3_logger = None
 
     @property
     def searcher(self):
@@ -87,10 +98,29 @@ class PyseriniRetriever(Task):
                 fb_terms = self.config.fb_terms
                 fb_docs = self.config.fb_docs
                 weight = self.config.original_query_weight
-                self._searcher.set_rm3(fb_terms, fb_docs, weight)
+                logging = self.config.rm3_logging
+                self._searcher.set_rm3(fb_terms, fb_docs, weight, logging)
+                if logging:
+                    self._setup_rm3_logging()
                 LOGGER.info(f'Adding RM3: fb_terms={fb_terms}, fb_docs={fb_docs}, original_query_weight={weight}')
 
         return self._searcher
+
+    def _setup_rm3_logging(self):
+        # remove the java console logger and replace it with a file logger
+        log_file = str(self.run_path / 'rm3.log')
+        context = self.java.cast(self.java.ContextManager, self.java.LogManager.getContext(False))
+        config = context.getConfiguration()
+        logging_config = config.getLoggerConfig(self.java.LogManager.ROOT_LOGGER_NAME)
+        layout = self.java.PatternLayout.createLayout("%d %p %m%n", None, None, None,
+                                                      self.java.Charset.defaultCharset(), False, False, None, None)
+        appender = self.java.FileAppender.createAppender(log_file, "false", "false", "File", "true", "false",
+                                                         "false", "4000", layout, None, "false", None, config)
+        appender.start()
+        logging_config.clearAppenders()
+        logging_config.addAppender(appender, self.java.Level.INFO, None)
+        self.rm3_logger = self.java.LogManager.getLogger("patapasco")
+        self.java.Rm3Reranker.LOG = self.rm3_logger
 
     def begin(self):
         try:
@@ -110,6 +140,9 @@ class PyseriniRetriever(Task):
         """
 
         hits = self.searcher.search(query.query, k=self.number)
+        # we have to do this after we search so that the java logger gets initialized before we use it
+        if self.rm3_logger:
+            self.java.Rm3Reranker.LOG.info(f"Query ID: {query.id}")
         LOGGER.debug(f"Retrieved {len(hits)} documents for {query.id}: {query.query}")
         results = [Result(hit.docid, rank, hit.score) for rank, hit in enumerate(hits)]
         return Results(query, self.lang, str(self), results)
